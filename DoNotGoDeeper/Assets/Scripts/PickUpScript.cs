759d2d31188c0555f6f5b77fd1736ae252feb91b
fmarque <104608984+fmarque@users.noreply.github.com>
2026-03-05 20:59:46 -0500

    some base object interactions
    +added scripts for light toggles and object pick up
    
    + added ability to pick up objects and throw them
    
    + added light switch button
    
    + walk speed and sprint speed values were changed but only for testing
    
    + added button, interactable cubes, and spot light objects
    
    + added text mesh packages so when player gets close to certain objects, they receive instruction on the screen
    
    - need to add door mechanics still
    
    - need to add audio and animation for button toggles

---
+++ DoNotGoDeeper/Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+    public float throwForce = 500f;
+    public float pickUpRange = 5f;
+    private float rotationSensitivity = 1f;
+    private GameObject heldObj;
+    private Rigidbody heldObjRb;
+    private bool canDrop = true;
+    private int LayerNumber;
+
+    void Start()
+    {
+        LayerNumber = LayerMask.NameToLayer("holdLayer");
+        Debug.Log("PickUpScript started on: " + gameObject.name);
+        Debug.Log("holdLayer index: " + LayerNumber);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed!");
+
+            if (heldObj == null)
+            {
+                Debug.Log("Casting ray from: " + transform.position + " direction: " + transform.TransformDirection(Vector3.forward));
+
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+                    Debug.Log("Ray hit: " + hit.transform.gameObject.name + " | Tag: " + hit.transform.gameObject.tag);
+
+                    if (hit.transform.gameObject.tag == "canPickUp")
+                    {
+                        PickUpObject(hit.transform.gameObject);
+                    }
+                    else
+                    {
+                        Debug.Log("Object hit but tag is wrong. Expected 'canPickUp', got: " + hit.transform.gameObject.tag);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Raycast hit NOTHING within range " + pickUpRange);
+                }
+            }
+            else
+            {
+                if (canDrop == true)
+                {
+                    StopClipping();
+                    DropObject();
+                }
+            }
+        }
+
+        if (heldObj != null)
+        {
+            MoveObject();
+            RotateObject();
+            if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop == true)
+            {
+                StopClipping();
+                ThrowObject();
+            }
+        }
+    }
+
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>())
+        {
+            heldObj = pickUpObj;
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>();
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform;
+            heldObj.layer = LayerNumber;
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+        else
+        {
+            Debug.Log("Object has no Rigidbody! Can't pick up.");
+        }
+    }
+
+    void DropObject()
+    {
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+
+    void MoveObject()
+    {
+        heldObj.transform.position = holdPos.transform.position;
+    }
+
+    void RotateObject()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            canDrop = false;
+            float XaxisRotation = Input.GetAxis("Mouse X") * rotationSensitivity;
+            float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSensitivity;
+            heldObj.transform.Rotate(Vector3.down, XaxisRotation);
+            heldObj.transform.Rotate(Vector3.right, YaxisRotation);
+        }
+        else
+        {
+            canDrop = true;
+        }
+    }
+
+    void ThrowObject()
+    {
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObjRb.AddForce(transform.forward * throwForce);
+        heldObj = null;
+    }
+
+    void StopClipping()
+    {
+        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
+        if (hits.Length > 1)
+        {
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
+        }
+    }
+}
---
+++ DoNotGoDeeper/Assets/Scripts/lightonoff.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightonoff : MonoBehaviour
+{
+    public GameObject txtToDisplay;
+    public GameObject lightorobj;
+    public float detectionRange = 20f;
+
+    private bool PlayerInZone;
+    private Transform player;
+
+    private void Start()
+    {
+        PlayerInZone = false;
+        txtToDisplay.SetActive(false);
+        player = GameObject.FindWithTag("Player").transform;
+        
+        if (player == null)
+            Debug.Log("ERROR: No object tagged Player found!");
+        else
+            Debug.Log("Player found: " + player.name);
+            
+        if (lightorobj == null)
+            Debug.Log("ERROR: lightorobj not assigned in Inspector!");
+            
+        if (txtToDisplay == null)
+            Debug.Log("ERROR: txtToDisplay not assigned in Inspector!");
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+        
+        float distance = Vector3.Distance(transform.position, player.position);
+        Debug.Log("Distance to player: " + distance);
+
+        if (distance < detectionRange)
+        {
+            if (!PlayerInZone)
+            {
+                txtToDisplay.SetActive(true);
+                PlayerInZone = true;
+                Debug.Log("Player entered zone!");
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("F pressed, toggling light!");
+                Light lightComponent = lightorobj.GetComponent<Light>();
+                lightComponent.enabled = !lightComponent.enabled;            }
+        }
+        else
+        {
+            if (PlayerInZone)
+            {
+                txtToDisplay.SetActive(false);
+                PlayerInZone = false;
+                Debug.Log("Player left zone!");
+            }
+        }
+    }
+}
