7852eefba220498979b33e66bb443c862a860e78
fmarque <104608984+fmarque@users.noreply.github.com>
2026-03-17 21:52:31 -0400

    adjusted player movement script
    + changed wall height from 1.5 to 1.2
    + adjusted player movement script for proper camera rotation

--- DoNotGoDeeper/Assets/Scripts/PlayerMovement.cs
+++ DoNotGoDeeper/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Camera playerCamera;
+    public Transform cameraPivot;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
     public float jumpPower = 7f;
@@ -88,7 +89,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            cameraPivot.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
